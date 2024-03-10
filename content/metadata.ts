d967331f3b8b9d97cbe8e277cf2c8da644361a80
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-03-10 09:03:15 +0800

    initial: initial setup

---
+++ content/metadata.ts
@@ -0,0 +1,5 @@
+export const data = {
+    title: "Yehezkiel Dio Sinolungan",
+    // prettier-ignore
+    description: "A software engineer with a passion for building digital experiences that are accessible and delightful with a focus on performance and user experience.",
+};
